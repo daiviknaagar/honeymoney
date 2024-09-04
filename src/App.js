9f94ddfdce249fbf250ee8a61e908fef47912c05
Jatin Singh <its.me.jatinsingh@gmail.com>
2024-09-04 22:00:52 +0530

    "resolving error in deploy"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import HomePage from './components/homepage';
 import ProductPage from './components/productPage';
 
